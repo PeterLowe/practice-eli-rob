28631a2ed578343dea3a7daba9f49d0dcba90c8f
(Student  C00302210) Robert Mc Gregor <C00302210@setu.ie>
2023-11-09 17:22:02 +0000

    I have altered your code. Pray I do not alter it further.

--- numbers/numbers/main.cpp
+++ numbers/numbers/main.cpp
@@ -1,9 +1,20 @@
 //eliska odd
+//rob even
 
 #include <iostream>
 
 int main()
 {
+	int inputNumber = 0;
+
+	std::cout << "WHAT IS YOUR NUMBER, HOOMAN?!" << std::endl;
+	std::cin >> inputNumber;
+
+	if (inputNumber % 2 == 0) // Use modulo to find if number is even or odd.
+	{
+		std::cout << "YOUR NUMBER IS EVEN! EVENNNNNN!" << std::endl;
+	}
+
 	system("pause");
 	return 1;
 }
