c3e04d69bd5d41af486bd46d7081e465f0b74b84
(Student  C00301768) Eliska Vrzalova <C00301768@setu.ie>
2023-11-09 17:28:49 +0000

    odds are here!

--- numbers/numbers/main.cpp
+++ numbers/numbers/main.cpp
@@ -4,6 +4,15 @@
 
 int main()
 {
+	int number = 6;
+	//user input
+	std::cout << "NUMBERRRR, NOOOOW!!!" << std::endl;
+	std::cin >> number;
+
+	if (number % == 1) {
+		std::cout << "The number was odd" << std::endl;
+	}
+
 	system("pause");
 	return 1;
 }
